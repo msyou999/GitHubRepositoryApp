7570bc92c3599fe9338105a5cb86dabbf6d24e37
Yoo MinSeo <msyou999@gmail.com>
2021-12-15 14:34:54 +0900

    Initial Commit

---
+++ GitHub/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GitHub
+//
+//  Created by SNPLAB on 2021/12/15.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
